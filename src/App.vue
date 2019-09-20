6aea9091e51c44d991e320377ad6d46a5065c2aa
Hanna Farag <hannafarag159@gmail.com>
2019-09-20 12:24:41 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Hello/>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+html, body {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+  width: 100%;
+  background-color: #f9c0b9;
+}
+</style>
